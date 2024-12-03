712fdffc0a5c64e787e7ff0d0eb2e0a0d77a150f
Katia Aresti <karesti@redhat.com>
2024-12-03 17:50:54 +0100

    initial commit

---
+++ runtime/src/main/java/org/infinispan/quarkus/embedded/runtime/InfinispanEmbeddedRuntimeConfig.java
@@ -0,0 +1,20 @@
+package org.infinispan.quarkus.embedded.runtime;
+
+import java.util.Optional;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+
+/**
+ * @author wburns
+ */
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+@ConfigMapping(prefix = "infinispan-embedded")
+public interface InfinispanEmbeddedRuntimeConfig {
+
+    /**
+     * The configured Infinispan embedded xml file which is used by the managed EmbeddedCacheManager and its Caches
+     */
+    Optional<String> xmlConfig();
+}
