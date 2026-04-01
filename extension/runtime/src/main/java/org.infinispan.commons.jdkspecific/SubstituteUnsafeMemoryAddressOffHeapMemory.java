ee100a0e4313e8e3551fac40f1e49f7398676259
Katia Aresti <karestig@ibm.com>
2026-04-01 12:51:32 +0200

    Remove JDK 22 substitution

--- extension/runtime/src/main/java/org.infinispan.commons.jdkspecific/SubstituteUnsafeMemoryAddressOffHeapMemory.java
+++ extension/runtime/src/main/java/org.infinispan.commons.jdkspecific/SubstituteUnsafeMemoryAddressOffHeapMemory.java
@@ -1,19 +1,9 @@
 package org.infinispan.commons.jdkspecific;
 
-import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.Delete;
 import com.oracle.svm.core.annotate.TargetClass;
 
-@TargetClass(UnsafeMemoryAddressOffHeapMemory.class)
+@TargetClass(className = "org.infinispan.commons.jdkspecific.UnsafeMemoryAddressOffHeapMemory")
+@Delete
 public final class SubstituteUnsafeMemoryAddressOffHeapMemory {
-
-    @Substitute
-    public long allocate(long bytes) {
-        return 0L;
-    }
-
-    @Substitute
-    public void free(long address) {
-
-    }
-
 }
