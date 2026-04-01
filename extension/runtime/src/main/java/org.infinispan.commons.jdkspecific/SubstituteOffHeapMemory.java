e21472fb4ac11ee7e08105812f8faadcd6cac7c4
Katia Aresti <karestig@ibm.com>
2026-04-01 12:51:32 +0200

    Remove JDK 22 substitution

--- extension/runtime/src/main/java/org.infinispan.commons.jdkspecific/SubstituteOffHeapMemory.java
+++ extension/runtime/src/main/java/org.infinispan.commons.jdkspecific/SubstituteOffHeapMemory.java
@@ -1,13 +1,9 @@
 package org.infinispan.commons.jdkspecific;
 
-import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.Delete;
 import com.oracle.svm.core.annotate.TargetClass;
 
-@TargetClass(OffHeapMemory.class)
+@TargetClass(className = "org.infinispan.commons.jdkspecific.OffHeapMemory")
+@Delete
 public final class SubstituteOffHeapMemory {
-
-    @Substitute
-    public static org.infinispan.commons.spi.OffHeapMemory getInstance() {
-        return UnsafeMemoryAddressOffHeapMemory.getInstance();
-    }
 }
